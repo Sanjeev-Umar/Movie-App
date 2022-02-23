406dfa500d0707c39e93cba1eaeccb5453bb7e19
Sanjeev <umarsanjeev@gmail.com>
2022-02-23 18:02:55 +0530

    add script file and temp img

---
+++ app.js
@@ -0,0 +1,5 @@
+// const API_KEY = "8cea1b9c";
+
+// fetch(`http://www.omdbapi.com/?s=dark&y=2012&page=1&apikey=${API_KEY}`)
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
